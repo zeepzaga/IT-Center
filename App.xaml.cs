0db1dad409d22f1b1078ed95c88d86de876f44fd
ОБПОУ КГПК <kgpk2@outlook.com>
2021-06-07 20:16:09 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IT_Center
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Произошла неизвестная ошибка!","Ошибка!",MessageBoxButton.OK,MessageBoxImage.Error);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using IT_Center.Pages;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace IT_Center
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dispatcherTimer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("ru-RU");
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
+            TbDateTime.Text = $"{DateTime.Now:dd MMMM yyyy hh:mm}";
+            dispatcherTimer.Start();
+            MainFrame.Navigate(new MainMenuPage());
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            TbDateTime.Text = $"{DateTime.Now:dd MMMM yyyy hh:mm}";
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            var page = MainFrame.Content as Page;
+            if (page is AutorizationPage)
+            {
+                SpLogout.Visibility = Visibility.Collapsed;
+                GridLine.Visibility = Visibility.Collapsed;
+                TblBack.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                SpLogout.Visibility = Visibility.Visible;
+                GridLine.Visibility = Visibility.Visible;
+                TblBack.Visibility = Visibility.Visible;
+            }
+
+        }
+
+        private void TblBack_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var page = MainFrame.Content as Page;
+            if (page is MainMenuPage)
+            {
+                TblLogout_MouseLeftButtonDown(null, null);
+                return;
+            }
+            if (MainFrame.CanGoBack)
+                MainFrame.GoBack();
+        }
+
+        private void TblLogout_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(MessageBox.Show("Выйти из аккаунта?", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                MainFrame.Navigate(new AutorizationPage());
+            }
+        }
+    }
+}
